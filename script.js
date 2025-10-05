0be14b5ebf0f32453bad261a67565a1cc38c01f3
jalvarengavelasquez-dot <jalvarengavelasquez@gmail.com>
2025-10-05 15:18:34 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function mostrarMensaje() {
-  alert("Bienvenido al Banco Astrelia. Estamos aquí para ayudarte a crecer con confianza.");
-}
+document.getElementById("loanForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const monto = parseFloat(document.getElementById("monto").value);
+  const semanas = parseInt(document.getElementById("semanas").value);
+  const tasa = 0.0155; // 1.55% semanal
+
+  if (monto > 0 && semanas > 0) {
+    // Fórmula de PAGO
+    const pago = (tasa * monto) / (1 - Math.pow(1 + tasa, -semanas));
+
+    document.getElementById("resultado").innerHTML = 
+      `Tu pago semanal será de: <br><strong>L. ${pago.toFixed(2)}</strong>`;
+  } else {
+    document.getElementById("resultado").innerHTML = 
+      "Por favor ingresa valores válidos.";
+  }
+});
